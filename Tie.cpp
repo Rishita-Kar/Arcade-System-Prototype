6cb7f7299800ea5baa72c0930518277cfb07af3a
Rishita-Kar <52338116+Rishita-Kar@users.noreply.github.com>
2019-06-28 20:08:42 -0700

    Add files via upload

---
+++ Tie.cpp
@@ -0,0 +1,3 @@
+#include "stdafx.h"
+#include "Tie.h"
+
---
+++ Tie.h
@@ -0,0 +1,163 @@
+#pragma once
+
+namespace ArcadeMenu {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Tie
+	/// </summary>
+	public ref class Tie : public System::Windows::Forms::Form
+	{
+	public:
+		Tie(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Tie()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: AxWMPLib::AxWindowsMediaPlayer^  axWindowsMediaPlayer1;
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Tie::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->axWindowsMediaPlayer1 = (gcnew AxWMPLib::AxWindowsMediaPlayer());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::GrayText;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Candara", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::Gold;
+			this->label1->Location = System::Drawing::Point(22, 17);
+			this->label1->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(343, 45);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Gosh... both players,";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::SystemColors::GrayText;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Candara", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::Gold;
+			this->label2->Location = System::Drawing::Point(53, 81);
+			this->label2->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(340, 45);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"are a pair of losers...";
+			this->label2->Click += gcnew System::EventHandler(this, &Tie::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::SystemColors::GrayText;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Candara", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::Gold;
+			this->label3->Location = System::Drawing::Point(51, 159);
+			this->label3->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(163, 63);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"IT IS A";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::SystemColors::GrayText;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Candara", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::Color::Gold;
+			this->label4->Location = System::Drawing::Point(95, 249);
+			this->label4->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(174, 63);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"TIE !! :(";
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			this->axWindowsMediaPlayer1->Enabled = true;
+			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(307, 159);
+			this->axWindowsMediaPlayer1->Name = L"axWindowsMediaPlayer1";
+			this->axWindowsMediaPlayer1->OcxState = (cli::safe_cast<System::Windows::Forms::AxHost::State^>(resources->GetObject(L"axWindowsMediaPlayer1.OcxState")));
+			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(243, 94);
+			this->axWindowsMediaPlayer1->TabIndex = 4;
+			// 
+			// Tie
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->ClientSize = System::Drawing::Size(607, 394);
+			this->Controls->Add(this->axWindowsMediaPlayer1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(6);
+			this->Name = L"Tie";
+			this->Text = L"Tie";
+			this->Load += gcnew System::EventHandler(this, &Tie::Tie_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Tie_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
